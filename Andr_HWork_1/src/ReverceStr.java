96b43cf01869e5f81263d84509d76b7bbd537a6c
XPMUser <XPMUser@VPC-SFA-11>
2014-10-03 09:59:04 +0300

    zad 1 oki

--- Andr_HWork_1/src/ReverceStr.java
+++ Andr_HWork_1/src/ReverceStr.java
@@ -3,7 +3,16 @@ public class ReverceStr {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String a = "123456789";
+		System.out.println(reverceMe(a));
+		 
 	}
-
+	private static String reverceMe(String a){
+		String b="";  
+		for (int i = a.length()-1; i>=0; i--) {
+			b = b + a.substring(i,i+1);
+		}
+		return b;
+	}
+	 
 }
